be3e8d05e873bed0bbff8d32341b9d2731243b5e
Link <lenkgit@gmail.com>
2021-12-28 17:32:08 -0500

    ADD: NestedMarkdown for underlines

--- BetterMarkdown/src/main/java/net/navibot/aliucord/plugins/BetterMarkdown.kt
+++ BetterMarkdown/src/main/java/net/navibot/aliucord/plugins/BetterMarkdown.kt
@@ -4,14 +4,11 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.text.Editable
-import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.*
-import com.aliucord.Utils
 import com.aliucord.Utils.log
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.discord.utilities.textprocessing.SpannableUtilsKt
 import com.discord.widgets.chat.input.WidgetChatInputEditText
 import com.lytefast.flexinput.widget.FlexEditText
 import de.robv.android.xposed.XC_MethodHook
@@ -22,13 +19,13 @@ class BetterMarkdown : Plugin() {
     private val listenerMethod = "setOnTextChangedListener"
 
     // list of markdowns
-    private val patterns = arrayOf(
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)\\`[^*]+\\`)"), BackgroundColorSpan(Color.parseColor("#2F3136"))),
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)(\\*|_)[^*_]+(\\*|_))"), StyleSpan(Typeface.ITALIC)),
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)\\*{2}[^*]+\\*{2})"), StyleSpan(Typeface.BOLD)),
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)\\*{3}[^*]+\\*{3})"), StyleSpan(Typeface.BOLD_ITALIC)),
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)~{2}[^~]+~{2})"), StrikethroughSpan()),
-        Pair<Regex, CharacterStyle>(Regex("((^|\\s)_{2}[^_]+_{2})"), UnderlineSpan())
+    private val styles = arrayOf(
+        Pair<Regex, CharacterStyle>(Regex("((^|[^*])\\`[^*]+\\`)"), BackgroundColorSpan(Color.parseColor("#2F3136"))),
+        Pair<Regex, CharacterStyle>(Regex("((^|[^*])(\\*|_)[^*_]+(\\*|_))"), StyleSpan(Typeface.ITALIC)),
+        Pair<Regex, CharacterStyle>(Regex("((^|[^*])\\*{2}[^*]+\\*{2})"), StyleSpan(Typeface.BOLD)),
+        Pair<Regex, CharacterStyle>(Regex("((^|[^*])\\*{3}[^*]+\\*{3})"), StyleSpan(Typeface.BOLD_ITALIC)),
+        Pair<Regex, CharacterStyle>(Regex("((^|[^~])~{2}[^~]+~{2})"), StrikethroughSpan()),
+        Pair<Regex, CharacterStyle>(Regex("((^|[^_])_{2}[^_]+_{2})"), UnderlineSpan())
     )
 
     override fun start(context: Context) {
@@ -50,7 +47,7 @@ class BetterMarkdown : Plugin() {
                                 val data = s.toString()
 
                                 // Apply any found patterns
-                                patterns.forEach { pattern ->
+                                styles.forEach { pattern ->
                                     pattern.first.findAll(data, 0).forEach { match ->
                                         setSpan(s, pattern.second, data, match.groupValues.first().trim())
                                     }
@@ -70,13 +67,22 @@ class BetterMarkdown : Plugin() {
     }
 
     override fun stop(context: Context) {
-
+        patcher.unpatchAll()
     }
 
     private fun setSpan(editable: Editable, span: CharacterStyle, data: String, markdown: String) {
         val range = getRange(data, markdown)
-
         editable.setSpan(CharacterStyle.wrap(span), range.start, range.last, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        if (span is UnderlineSpan) {
+            styles.forEach { pair ->
+                if (pair.second !is UnderlineSpan) {
+                    pair.first.findAll(data, 0).forEach { match ->
+                        setSpan(editable, pair.second, data, match.groupValues.first().trim())
+                    }
+                }
+            }
+        }
     }
 
     private fun getRange(data: String, markdown: String) : IntRange {
