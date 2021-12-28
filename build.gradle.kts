fd28d2f17e5057bb4fa04c2a98b62b5d4f60b743
Link <lenkgit@gmail.com>
2021-12-27 20:06:25 -0500

    Changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.github.Aliucord:gradle:main-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
 
