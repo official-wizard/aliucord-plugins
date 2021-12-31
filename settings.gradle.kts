c6c7ddfa013d4a2cfa604c8a4eb3624f7343fb1c
Link <lenkgit@gmail.com>
2021-12-31 06:28:14 -0500

    Initial Release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,4 +10,10 @@ project(":CopyPlease").projectDir = File("./CopyPlease")
 include(":Obliterate")
 project(":Obliterate").projectDir = File("./Obliterate")
 
+include(":Favoritism")
+project(":Favoritism").projectDir = File("./Favoritism")
+
+include(":LetThereBeColors")
+project(":LetThereBeColors").projectDir = File("./LetThereBeColors")
+
 rootProject.name = "aliucord-plugins"
