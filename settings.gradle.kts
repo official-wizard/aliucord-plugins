758924e5be3de2fbad5a724b02422f69957eca42
Link <lenkgit@gmail.com>
2021-12-31 06:28:43 -0500

    Initial Release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,6 @@ project(":CopyPlease").projectDir = File("./CopyPlease")
 include(":Obliterate")
 project(":Obliterate").projectDir = File("./Obliterate")
 
-include(":Favoritism")
-project(":Favoritism").projectDir = File("./Favoritism")
-
 include(":LetThereBeColors")
 project(":LetThereBeColors").projectDir = File("./LetThereBeColors")
 
